94c3c439839879b54c9c87287b84b36e30d0b355
Lavander40 <a.kirill.03@mail.ru>
2024-07-27 01:43:44 +0300

    hosts configuraion added

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,12 +18,14 @@ type Config struct {
 // MustLoad returns formed configuration for the application or stops completely it with fatal error 
 func MustLoad() *Config {
 	var c Config
-	var pUser, pPassword string
+	var pUser, pPassword, pHost, kHost string
 
 	// reading provided flags
 	flag.StringVar(&c.LogLevel, "log", "debug", "set level for logger")
 	flag.StringVar(&pUser, "user", "", "user for postgre")
 	flag.StringVar(&pPassword, "pass", "", "password for postgre")
+	flag.StringVar(&pHost, "phost", "", "host for postgre")
+	flag.StringVar(&kHost, "khost", "", "host for kafka")
 	flag.Parse()
 
 	// if flags are empty then read from env variables
@@ -36,6 +38,12 @@ func MustLoad() *Config {
 	if pPassword == "" {
 		pPassword = os.Getenv("POSTGRES_PASSWORD")
 	}
+	if pHost == "" {
+		pHost = os.Getenv("POSTGRES_HOST")
+	}
+	if kHost == "" {
+		kHost = os.Getenv("KAFKA_HOST")
+	}
 
 	// error on required parameters
 	if pUser == "" || pPassword == "" {
@@ -43,8 +51,8 @@ func MustLoad() *Config {
 	}
 
 	c.ApiPort = "8090"
-	c.PostgreConnect = fmt.Sprintf("postgres://%s:%s@local:5432/messages?sslmode=disable", pUser, pPassword)
-	c.KafkaConnect = "kafka:9092"
+	c.PostgreConnect = fmt.Sprintf("postgres://%s:%s@%s:5432/messages?sslmode=disable", pUser, pPassword, pHost)
+	c.KafkaConnect = fmt.Sprintf("%s:9092", kHost)
 
 	return &c
 }
